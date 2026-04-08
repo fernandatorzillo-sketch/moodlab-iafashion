05a73914066afe2a1bde08f31c817d6cbee29909
fernandatorzillo-sketch <fernanda.torzillo@icloud.com>
2026-04-08 16:18:46 -0300

    auto update

--- auto_push.ps1
+++ auto_push.ps1
@@ -1,6 +1,12 @@
 while ($true) {
     git add .
-    git commit -m "auto update" 2>$null
-    git push origin main
+
+    git status --porcelain | ForEach-Object {
+        if ($_ -notmatch "data_models/orders_vtex_24m.json") {
+            git commit -m "auto update" 2>$null
+            git push origin main
+        }
+    }
+
     Start-Sleep -Seconds 60
 }
